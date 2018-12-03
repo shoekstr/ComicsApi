467609262ec75c60f4082d3638b5ac2009c88026
shoekstra <shoekstra@TEKMOBILE11>
2018-12-03 13:56:05 -0500

    Added config and index.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const environments = {}
+
+environments.staging = {
+  httpsPort: 3000,
+  envName: 'staging'
+}
+
+environments.production = {
+  httpsPort: 5000,
+  envName: 'production'
+}
+const currentEnvironment = typeof(process.env.NODE_ENV) === 'string'
+  ? process.env.NODE_ENV.toLowerCase()
+  : ''
+
+const environmentToExport = typeof(environments[currentEnvironment]) === 'object'
+  ? environments[currentEnvironment]
+  : environments.staging
+
+module.exports = environmentToExport
---
+++ index.js
@@ -0,0 +1,56 @@
+const https = require('https')
+const url = require('url')
+const fs = require('fs')
+const StringDecoder = require('string_decoder').StringDecoder
+
+const config = require('./config')
+
+const httpServerOptions = {
+    key: fs.readFileSync('./http/key.pem'),
+    cert: fs.readFileSync('./http/cert.pem')
+}
+
+const httpsServer = https.createServer(httpServerOptions, (req, res) => {
+    unifiedServer(req, res)
+})
+
+httpsServer.listen(config.httpsPort, () => {
+    console.log('The HTTPS server is running on port ' + config.httpsPort)
+})
+
+const unifiedServer = (req, res) => {
+    const parsedUrl = url.parse(req.url, true)
+
+    const path = parsedUrl.pathName
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+    const queryStringObject = parsedUrl.query
+
+    const method = req.method.toLowerCase()
+
+    const headers = req.headers
+
+    const decoder = new StringDecoder('utf-8')
+    let buffer = ''
+
+    req.on('data', (data) => {
+        buffer += decoder.write(data)
+    })
+
+    req.on('end', () => {
+        buffer += decoder.end()
+    })
+
+    const data = {
+        trimmedPath: trimmedPath,
+        queryStringObject: queryStringObject,
+        method: method,
+        headers: headers,
+        payload: buffer
+    }
+
+    console.log(data)
+
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(200)
+}
